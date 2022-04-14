b6150be9a886cc4c13b3b77041824411953c6376
aban hasan <abanstampy@gmail.com>
2022-04-14 19:13:01 +0530

    created selection mechanism for task list

--- commands/taskList.js
+++ commands/taskList.js
@@ -1,10 +1,10 @@
 const Skill = require("../objects/skill.js");
 const Task = require("../objects/task.js");
-const { MessageEmbed } = require("discord.js");
+const {MessageActionRow, MessageSelectMenu,MessageEmbed} = require("discord.js");
 const romanise = require("../modules/romanNumeralHelper");
 
 // Initialize task objects from skill objects.
-// The Math.random() > 0.5 part just decides a random value for whether the task has been completed or not, 
+// The Math.random() > 0.5 part just decides a random value for whether the task has been completed or not,
 // as it's just a template for now.
 const tasks = [ //eslint-disable-line no-unused-vars
   new Skill("reading.png","Reading", 4, "READ 30m", "day", "x1 MONTH",1, 800),
@@ -35,15 +35,27 @@ exports.run = (client, message, args, level) => { // eslint-disable-line no-unus
     .setColor("#1071E5")
     .addField("Daily Tasks", dailyTaskStrings.join("\n"))
     .addField("No Deadline", otherTaskStrings.join("\n"));
-
-  message.channel.send({ embeds: [embed] });
+  const dropDownBox = new MessageActionRow().addComponents(
+    new MessageSelectMenu().setCustomId("tasks-selection-box").setPlaceholder("Select a task").addOptions(
+      tasks.map(
+        tasks => {
+          return {
+            label: tasks.skill.title,
+            description: tasks.skill.goal,
+            value: tasks.skill.title
+          };
+        }
+      )
+    )
+  );
+  message.channel.send({ embeds: [embed], components: [dropDownBox] });
 };
 
 function formatTask(task, idx) {
-  let completedEmoji = task.completed ? ":white_check_mark:": ":x:";
-  let levelRoman = romanise(task.skill.level);
-  let frequencyFormat = `${task.skill.frequency} ${task.skill.frequency === 1 ? "time" : "times"}/${task.skill.time}`;
-  
+  const completedEmoji = task.completed ? ":white_check_mark:": ":x:";
+  const levelRoman = romanise(task.skill.level);
+  const frequencyFormat = `${task.skill.frequency} ${task.skill.frequency === 1 ? "time" : "times"}/${task.skill.time}`;
+
   return `${completedEmoji} | **${idx + 1}. ${task.skill.title} ${levelRoman} (${frequencyFormat})**: ${task.skill.goal}`;
 }
 
