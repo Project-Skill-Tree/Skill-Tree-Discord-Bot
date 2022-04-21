a60f5a3d164184e3272ee3aa110033a64a178d46
aban hasan <abanstampy@gmail.com>
2022-04-21 10:13:16 +0530

    formatting

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -16,7 +16,14 @@ module.exports = (client,member) => {
   const embed = new MessageEmbed()
     .setTitle("Welcome to Skill Tree")
     .setColor(`#${configurations.primary}`)
-    .setDescription(`This project aims to gamify self improvement, using a simple modifiable Habit Tracker.\nLevel up in real life by completing tasks in "trees" created by the community. think of it like an in-game focus tree where you earn XP by doing missions.\n\n**How do i use the bot?**\nYou can set up your skill tree account using the \`~setup\` command, which can also be used in the Skill Tree app (not released yet!)\nlook through the \`~help\` menu to learn more about my features.\n\n**resources**\nInvite a friend using the invite link: ${configurations.invite_link} \nWebsite: ${configurations.website}`)
+    .setDescription(`This project aims to gamify self improvement, using a simple modifiable Habit Tracker.\n
+    Level up in real life by completing tasks in "trees" created by the community. think of it like an in-game focus tree where you earn XP by doing missions.\n\n
+    **How do i use the bot?**\n
+    You can set up your skill tree account using the \`~setup\` command, which can also be used in the Skill Tree app (not released yet!)\n
+    look through the \`~help\` menu to learn more about my features.\n\n
+    **resources**\n
+    Invite a friend using the invite link: ${configurations.invite_link} \n
+    Website: ${configurations.website}`)
     .setThumbnail(client.user.avatarURL)
     // linked image is the skill tree complete PDF iamge
     .setImage("https://media.discordapp.net/attachments/953955545012920370/953968687751245824/SELF_IMPROVEMENT_TECH_TREE.png?width=1499&height=857");
