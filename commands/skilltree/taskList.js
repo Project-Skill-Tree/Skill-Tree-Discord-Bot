53a0dbcb0bff501d808b4faa8f0b0801fdaa1043
aban hasan <abanstampy@gmail.com>
2022-06-06 07:54:07 +0530

    Update taskList.js

--- commands/skilltree/taskList.js
+++ commands/skilltree/taskList.js
@@ -53,7 +53,7 @@ async function createTaskList(client, message, tasks, userID) {
         .setLabel("TODAY")
         .setStyle("SECONDARY")
         .setDisabled(true));
-  const msg = await message.reply({ embeds: [embed], components: [row] });
+  const msg = await message.reply({ embeds: [embed], components: [dropDownBox,row] });
 
   const collector = msg.createMessageComponentCollector({time: 30000 });
 
