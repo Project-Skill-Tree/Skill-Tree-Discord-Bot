73ecb48d9ee1424225d7a2d63d8b6480aec5220d
Sean Hardy <seanjhardy@icloud.com>
2022-07-05 14:15:08 +1000

    Fixed page

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -17,9 +17,6 @@ module.exports = async (client, message) => {
   // If there is no guild, get default conf (DMs)
   const settings = message.settings = getSettings(message.guild);
 
-  //Ignore any channel which isn't #testing, EXCEPT DMs
-  if (message.guild !== null && message.guild.id !== "953924192259170335") return;
-
   // Checks if the bot was mentioned via regex, with no message after it,
   // returns the prefix. The reason why we used regex here instead of
   // message.mentions is because of the mention prefix later on in the
