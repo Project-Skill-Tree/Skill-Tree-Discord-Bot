4a4aa367226d39736d68755b0d0594ea5891e5b4
multiii <72256091+multiii@users.noreply.github.com>
2022-08-07 23:43:31 +0530

    Patch 4 (#95)
    * Update CommandInteraction.js
    
    * Update index.js
    
    * Update and rename CommandInteraction.js to BaseCommandInteraction.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { ShardingManager } = require("discord.js");
 require("./structures/TextChannel").run();
 require("./structures/Message").run();
 require("./structures/MessageComponentInteraction").run();
-require("./structures/CommandInteraction").run();
+require("./structures/BaseCommandInteraction").run();
 
 const manager = new ShardingManager("./bot.js", { token: process.env.DISCORD_TOKEN});
 
